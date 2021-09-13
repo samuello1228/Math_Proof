620708752380db2ccaa9f086af32a77a3f54767a
Samuel Lo <locheukyee@hotmail.com>
2021-09-14 02:51:08 +0800

    rewrite logic.tex

---
+++ Math_Proof/Proposition.hpp
@@ -0,0 +1,17 @@
+//
+//  Proposition.hpp
+//  Math_Proof
+//
+//  Created by Samuel on 13/9/2021.
+//  Copyright © 2021 Samuel. All rights reserved.
+//
+
+#ifndef Proposition_hpp
+#define Proposition_hpp
+
+class Proposition
+{
+    
+};
+
+#endif /* Proposition_hpp */
