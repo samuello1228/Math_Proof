b9aecff1427a4b86cdbc540fafc5c28200713126
Samuel Lo <locheukyee@hotmail.com>
2022-06-13 19:17:45 +0800

    format main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,17 +17,17 @@ int main()
     logic();
     set();
     
-    for(long i=0;i<Definition::All_Definition.size();i++)
+    for(long i = 0; i < Definition::All_Definition.size(); i++)
     {
         delete Definition::All_Definition[i];
     }
     
-    for(long i=0;i<Axiom::All_Axiom.size();i++)
+    for(long i = 0; i < Axiom::All_Axiom.size(); i++)
     {
         delete Axiom::All_Axiom[i];
     }
     
-    for(long i=0;i<Proposition::All_Proposition.size();i++)
+    for(long i = 0; i < Proposition::All_Proposition.size(); i++)
     {
         delete Proposition::All_Proposition[i];
     }
